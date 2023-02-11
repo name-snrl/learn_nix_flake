627b5d2435fab01471911b22b49f6d99ec7fa426
name_snrl <Demogorgon-74@ya.ru>
2023-02-11 16:41:18 +0600

    configuration: add default user

--- configuration/flake.nix
+++ configuration/flake.nix
@@ -92,7 +92,20 @@
               };
 
               services.openssh.enable = true;
-              users.users.root.openssh.authorizedKeys.keyFiles = [ inputs.ssh-keys ];
+              users = {
+                mutableUsers = false;
+                users = {
+                  root.openssh.authorizedKeys.keyFiles = [ inputs.ssh-keys ];
+                  default = {
+                    openssh.authorizedKeys.keyFiles = [ inputs.ssh-keys ];
+                    name = "name_snrl";
+                    hashedPassword =
+                      "$6$6US0iMDXE1K7wj9g$2/JKHfX4VfNETELdt4dTlTUzlmZAmvP4XfRNB5ORVPYNmi6.A4EWpSXkpx/5PrPx1J/LaA41n2NDss/R0Utqh/";
+                    isNormalUser = true;
+                    extraGroups = [ "wheel" ];
+                  };
+                };
+              };
 
               programs = {
 
