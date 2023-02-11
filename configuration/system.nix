6d5e9dc6c5a125701fbea8f99289f3d6534033f9
name_snrl <Demogorgon-74@ya.ru>
2023-02-11 18:00:20 +0600

    configuration: add sudoers rules

--- configuration/system.nix
+++ configuration/system.nix
@@ -24,6 +24,14 @@
     };
   };
 
+  security.sudo.extraRules = [{
+    users = [ config.users.users.default.name ];
+    commands = [
+      { options = [ "NOPASSWD" ]; command = "/nix/store/*/bin/switch-to-configuration"; }
+      { options = [ "NOPASSWD" ]; command = "/run/current-system/sw/bin/nix-env"; }
+    ];
+  }];
+
   programs = {
     nano.syntaxHighlight = false;
     less.enable = lib.mkForce false;
